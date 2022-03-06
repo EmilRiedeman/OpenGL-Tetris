e4a1e709fa120241f931f574e5097aa5556c12bc
Emil <riedeman.emil@gmail.com>
2022-03-06 18:53:11 +0100

    copied code from website

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
-#include "GLFW/glfw3.h"
 #include "GL/glew.h"
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewInit();
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
